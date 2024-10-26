21a19d2c61162bda9e12614379c6113922275187
Blair Kiel <jblairkiel@gmail.com>
2024-10-25 23:03:58 -0500

    udpate

--- proj/webgl_ellipses/webgl.js
+++ proj/webgl_ellipses/webgl.js
@@ -1,15 +1,16 @@
 p5.disableFriendlyErrors = true // disables FES jperformance increase
 
-var CANVAS_SIZE_W = 1860
-var CANVAS_SIZE_H = 900
+var CANVAS_SIZE_W = 600
+var CANVAS_SIZE_H = 500
 var custom_fps = 0
-
+let font;  
 class Ellipse {
   constructor () {
-    this.rand_x = Math.random() * (CANVAS_SIZE_W - 0)
-    this.rand_y = Math.random() * (CANVAS_SIZE_H - 0)
+    this.rand_x = Math.ceil(Math.random() * 300) * (Math.round(Math.random()) ? 1 : -1)
+    this.rand_y = Math.ceil(Math.random() * 300) * (Math.round(Math.random()) ? 1 : -1)
+    this.rand_z = Math.ceil(Math.random() * 300) * (Math.round(Math.random()) ? 1 : -1)
     this.rand_dia = Math.random() * (80 - 0)
-    this.dia_offset = Math.random() * (40 - 0)
+    this.dia_offset = Math.random() * (20 - 4) - 4
     var col_r = Math.floor(Math.random() * (255 - 0))
     var col_g = Math.floor(Math.random() * (255 - 0))
     var col_b = Math.floor(Math.random() * (255 - 0))
@@ -26,7 +27,7 @@ class Ellipse {
   }
 
   get_attrs () {
-    return [rand_x, rand_y, rand_dia, dia_offset]
+    return [rand_x, rand_y, rand_z, rand_dia, dia_offset]
   }
 
   draw () {
@@ -34,7 +35,9 @@ class Ellipse {
     fill(color(this.el_color[frame_picker][0], this.el_color[frame_picker][1], this.el_color[frame_picker][2]))
     var diameter = Math.sin(frameCount / 20 + this.dia_offset) * this.rand_dia + this.rand_dia
     //ellipse(this.rand_x, this.rand_y, diameter, diameter)
-    ellipse(this.rand_x, this.rand_y, diameter, diameter)
+    push();
+    translate(this.rand_x, this.rand_y, this.rand_z);
+    sphere(diameter)
   }
 }
 
@@ -54,8 +57,8 @@ class EllispeContainer {
   }
 }
 
-//let rand_elipse = new EllispeContainer(100); //Still 60
-let rand_elipse = new EllispeContainer(800); //pushing it
+let rand_elipse = new EllispeContainer(60); //Still 60
+//let rand_elipse = new EllispeContainer(800); //pushing it
 //let rand_elipse = generate_rand_elipse(CANVAS_SIZE_W, CANVAS_SIZE_H, 1000); // 20 fps
 
 function draw_fps () {
@@ -71,16 +74,23 @@ function draw_fps () {
     custom_fps = Math.floor(fps.toFixed(2))
   } 
   
+  textFont(font);
   text('FPS: ' + custom_fps, 1750, 60)
 }
 
+function preload(){
+  font = loadFont("../../assets/times.ttf")
+}
+
 function setup () {
-  createCanvas(CANVAS_SIZE_W, CANVAS_SIZE_H)
+  createCanvas(CANVAS_SIZE_W, CANVAS_SIZE_H, WEBGL)  
   frameRate(60)
 }
 
 function draw () {
   background(220)
+  lights();
+  orbitControl();
   rand_elipse.draw_elipse_list()
   draw_fps()
 }
